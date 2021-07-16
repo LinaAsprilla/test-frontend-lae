958a648f22a03bf0028e7fc8ab28cf48d1333e4c
Lina <linaasprilla@hotmail.com>
2021-07-16 16:47:07 -0500

    create routes login and register

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-function App() {
+import React from "react";
+import { Route, BrowserRouter } from "react-router-dom";
+import Register from "./container/Form/RegisterForm/";
+import Login from "./container/Form/LoginForm";
+
+const App = () => {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <Route exact path="/" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </React.Fragment>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;

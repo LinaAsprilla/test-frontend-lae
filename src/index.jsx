34e2bc49efe4f84961af2a1758d45067b0ff4671
Lina <linaasprilla@hotmail.com>
2021-07-18 08:37:37 -0500

    other changes

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createGlobalStyle } from "styled-components";
 import App from "./App";
+import "antd/dist/antd.css";
 
 const GlobalStyle = createGlobalStyle`
 body {
